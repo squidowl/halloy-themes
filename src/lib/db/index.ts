66b789680c94702eaf99e03faa6a018d20f04d92
Cory Forsstrom <cforsstrom18@gmail.com>
2025-03-17 20:50:47 -0700

    add dev .env

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,13 +1,12 @@
 import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-
-const connectionString = 'postgres://postgres:postgres@db.localtest.me:5432/main';
+import { CONNECTION_STRING } from '$env/static/private';
 
 neonConfig.fetchEndpoint = (host) => {
   const [protocol, port] = host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
   return `${protocol}://${host}:${port}/sql`;
 };
 
-export const db = drizzle({ client: neon(connectionString), casing: 'snake_case' });
+export const db = drizzle({ client: neon(CONNECTION_STRING), casing: 'snake_case' });
 
 export { themes } from '$lib/db/schema';
