8645fe8ca7aa63626f3eb582c381dc3bdc08c8e1
Cory Forsstrom <cforsstrom18@gmail.com>
2025-03-20 13:30:01 -0700

    Add vercel

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -1,12 +1,12 @@
 import { neon, neonConfig } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-http';
-import { CONNECTION_STRING } from '$env/static/private';
+import { NEON_DATABASE_URL } from '$env/static/private';
 
 neonConfig.fetchEndpoint = (host) => {
   const [protocol, port] = host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
   return `${protocol}://${host}:${port}/sql`;
 };
 
-export const db = drizzle({ client: neon(CONNECTION_STRING), casing: 'snake_case' });
+export const db = drizzle({ client: neon(NEON_DATABASE_URL), casing: 'snake_case' });
 
 export { themes } from '$lib/db/schema';
