ee46f5d63dc07fd39a1c6a009374d05d2d6212cd
Andrew Baldwin <andrew@boldfriend.org>
2026-09-16 14:33:16 -0700

    Update sidebar selection style in theme preview to match current (v2026.8) Halloy style.

--- src/lib/components/Window.svelte
+++ src/lib/components/Window.svelte
@@ -59,15 +59,15 @@
             <span>#glirc</span>
           </div>
           <div
-            class="rounded-lg px-6 py-1 bg-[var(--bg-color)] hover:bg-[var(--hover-bg-color)]"
-            style={`--bg-color: ${colors.buttons?.primary?.background_selected}; --hover-bg-color: ${colors.buttons?.primary?.background_selected_hover};`}
+            class="rounded-lg px-6 py-1 bg-[var(--bg-color)] hover:bg-[var(--hover-bg-color)] border"
+            style={`--bg-color: ${colors.buttons?.primary?.background_selected}; --hover-bg-color: ${colors.buttons?.primary?.background_selected_hover}; border-color: ${colors.buffer?.border_selected};`}
           >
             <span class="mr-0.5" style={`visibility: hidden;`}>•</span>
             <span>#halloy</span>
           </div>
           <div
             class="rounded-lg px-6 py-1 bg-[var(--bg-color)] hover:bg-[var(--hover-bg-color)]"
-            style={`--bg-color: ${colors.buttons?.primary?.background_hover}; --hover-bg-color: ${colors.buttons?.primary?.background_selected};`}
+            style={`--bg-color: ${colors.buttons?.primary?.background_selected}; --hover-bg-color: ${colors.buttons?.primary?.background_selected_hover};`}
           >
             <span class="mr-0.5" style={`visibility: hidden;`}>•</span>
             <span>#libera</span>
